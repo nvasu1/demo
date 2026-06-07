c585a3ed11572efc93be507bcb3b0c1637afd5fa
nvasu1 <onlinevasan@gmail.com>
2026-06-07 15:42:50 -0700

    Package name changed and added course.java

--- src/main/java/com/example/springboot/demo/DemoApplication.java
+++ src/main/java/com/example/springboot/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.test.demo;
+package com.example.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,3 +12,4 @@ public class DemoApplication {
 	}
 
 }
+  
