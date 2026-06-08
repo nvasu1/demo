f8c856f21f910a6a4794893e514d623fa777f72a
nvasu1 <onlinevasan@gmail.com>
2026-06-07 21:38:39 -0700

    demoapplication.java in dev1 branch

--- src/main/java/com/example/test/demo/DemoApplication.java
+++ src/main/java/com/example/test/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("Tomcat port was change from 8080 to 7080");
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Tomcat port was change from 8080 to 7080 .....");
 	}
 
 }
