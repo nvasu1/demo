3789f23f6274e0fcc12a26eb3a5f5b50dfeb919b
nvasu1 <onlinevasan@gmail.com>
2026-06-08 20:26:31 -0700

    Add title in course.java @ worldnew branch

--- src/main/java/com/example/springboot/course/Course.java
+++ src/main/java/com/example/springboot/course/Course.java
@@ -13,6 +13,15 @@ public class Course {
 		
 	private String author;
 	
+	private String title;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
 	public Course() {
 	}
